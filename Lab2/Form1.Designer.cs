a56ae5eb7c930fa29c41ac76bc9fdf9529240e28
nhathout <nhathout11@gmail.com>
2025-04-23 18:58:16 -0400

    FEATURE added lab 2 files to github

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(66, 17);
+            this.checkBox2.TabIndex = 0;
+            this.checkBox2.Text = "Connect";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 35);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 1;
+            this.checkBox3.Text = "Connect All";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Lab2
+{
+    public partial class Form1 : Form
+    {
+
+        private ArrayList coordinates = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                Point p = new Point(e.X, e.Y);
+                this.coordinates.Add(p);
+                this.Invalidate();
+            } 
+            else if (e.Button == MouseButtons.Right)
+            {
+                coordinates.Clear();
+                this.Invalidate();
+            }
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            const int WIDTH = 10;
+            const int HEIGHT = 10;
+            Graphics g = e.Graphics;
+
+            foreach(Point p in this.coordinates)
+            {
+                g.FillEllipse(Brushes.Black, p.X - WIDTH / 2, p.Y - WIDTH / 2, WIDTH, HEIGHT);
+            }
+
+            if(checkBox2.Checked && checkBox3.Checked)
+            {
+                MessageBox.Show("Error: Both check boxes can't be checked, either one or none must be checked.");
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+            }
+            else if (checkBox2.Checked)
+            {
+                for(int i = 0; i < this.coordinates.Count; i++)
+                {
+                    Point current = (Point)this.coordinates[i];
+                    Point next = (Point)this.coordinates[(i + 1) % coordinates.Count]; // % coordinates.Count makes the last point loop back around and connect to the first
+                    
+                    g.DrawLine(Pens.Black, current, next);
+                }
+            }
+            else if (checkBox3.Checked)
+            {
+                for(int i = 0; i < this.coordinates.Count; i++)
+                {
+                    for(int j = i + 1; j < coordinates.Count; j++)
+                    {
+                        g.DrawLine(Pens.Black, (Point)this.coordinates[i], (Point)this.coordinates[j]);
+                    }
+                }
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+    }
+}
